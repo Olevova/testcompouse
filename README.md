cdf087c731ed1d4d561a63d4d856e4a0660044f6
Olevova <olevova1983@gmail.com>
2023-09-05 08:42:19 +0300

    test in docker compouse

---
+++ README.md
@@ -0,0 +1,7 @@
+#simple test with docker
+1. Open Docker on your PC
+2. Jump in your framework (VS code or else)
+3. Clone repositore
+4. Open folder
+4. In terminal run command 'git install'
+5. After run command 'npm run test'
---
+++ test/simpleTest.js
@@ -0,0 +1,46 @@
+const { describe } = require("mocha");
+const { Builder, By } = require("selenium-webdriver");
+const should = require("chai").should();
+const chrome = require("selenium-webdriver/chrome");
+const waitOn = require("wait-on");
+
+
+describe("api simple test ", () => {
+    
+  let driver;
+  const textLocator = By.id('test');
+  
+  beforeEach(async () => {
+
+    await waitOn({
+      resources: ["http://selenium-hub:4444"],
+      timeout: 10000,
+    });
+  
+    const options = new chrome.Options();
+    options.addArguments("--incognito");
+    
+    driver = await new Builder()
+      .forBrowser("chrome")
+      .usingServer("http://selenium-hub:4444")
+      .setChromeOptions(options)
+      .build();
+
+  });
+
+  afterEach(async () => {
+
+    if (driver) {
+      await driver.quit();
+    };
+
+  });
+
+  it("test, find word", async () => {
+    
+      await driver.get("http://html-server");
+
+      const findeText = await driver.findElement(textLocator).getText();
+      findeText.should.to.equal('Its test time');
+  });
+});
